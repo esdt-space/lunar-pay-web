1823d1041051bd41967ddf187062bb418ad411a9
Alex <alex@devaccent.com>
2023-09-21 23:13:21 +0300

    Fix global is not defined error and added https support

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,34 @@
 import path from "path"
 import { defineConfig } from 'vite'
 import svgr from 'vite-plugin-svgr'
+import mkcert from 'vite-plugin-mkcert'
 import react from '@vitejs/plugin-react'
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svgr(), react()],
+  plugins: [svgr(), react(), mkcert()],
+  define: {
+    global: 'globalThis',
+  },
+  server: {
+    https: true,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: 'globalThis',
+      },
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          buffer: true,
+        }),
+      ],
     },
   },
 })
