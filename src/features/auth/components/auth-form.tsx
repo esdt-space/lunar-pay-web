a2c18d18351974ee0ea146d795e9300a876ec602
Alex <alex@devaccent.com>
2023-09-22 04:14:01 +0300

    Add auth components

---
+++ src/features/auth/components/auth-form.tsx
@@ -0,0 +1,56 @@
+import { useState } from 'react'
+import { Info } from 'lucide-react'
+
+import AppEnvironment from '@/environment/app.environment'
+
+import { Card } from "@/components/ui/card.tsx"
+
+import { AuthButtons } from '../components/auth-buttons'
+import { MobileConnectContent } from './partials/mobile-connect-content'
+
+type Props = {
+  callbackRoute: string
+}
+
+type FlowContentStep = 'mobile-wallet' | 'default'
+
+export function AuthForm(props: Props) {
+  const { callbackRoute } = props
+  const [flowStep, setFlowStep] = useState<FlowContentStep>('default')
+
+  return (
+    <div className={'flex flex-col flex-1 gap-8'}>
+        <Card className={'p-4 space-y-2 bg-muted/50'}>
+          <div className={'flex items-center gap-1'}>
+            <Info className={'w-4 h-4'} />
+            <p className={'text-sm font-medium'}>
+              Beware of scams and phishing attempts!
+            </p>
+          </div>
+          <p className={'text-xs text-muted-foreground'}>
+            Check the website link carefully:{' '}
+            <a className={'text-green-600 font-medium'} href={AppEnvironment.appUrl}>
+              {AppEnvironment.appUrl}
+            </a>
+          </p>
+        </Card>
+
+
+      {flowStep === 'default' && (
+        <>
+        <AuthButtons
+          callbackRoute={callbackRoute}
+          mobileAppButtonClickHandler={() => setFlowStep('mobile-wallet')}
+        />
+
+        <div className={'text-center'}>
+          <h3 className={'text-lg font-bold'}>New to MultiversX?</h3>
+          <div className={'text-muted-foreground'}>Easily create your own MultiversX wallet using xPortal</div>
+        </div>
+        </>
+      )}
+
+      {flowStep === 'mobile-wallet' && <MobileConnectContent logoutRoute={callbackRoute} />}
+    </div>
+  )
+}
