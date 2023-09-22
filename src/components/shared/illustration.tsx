220a20f025037996698e224b3f5d6eba08f66365
Alex <alex@devaccent.com>
2023-09-22 19:56:30 +0300

    Removed unused component

--- src/components/shared/illustration.tsx
+++
@@ -1,5 +0,0 @@
-import {ReactComponent as IllustrationImage} from '@/assets/media/illustration.svg';
-
-export function Illustration() {
-    return <IllustrationImage className={'object-cover mt-20'}  />
-}
