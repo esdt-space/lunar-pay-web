026e9e252799cfbe55267778c999b3cc418f7281
Alex <alex@devaccent.com>
2023-09-22 19:56:15 +0300

    Refactored auth screen components

--- src/screens/auth/authentication-screen.tsx
+++ src/screens/auth/authentication-screen.tsx
@@ -1,22 +1,23 @@
-import { AppIcon } from "@/components/shared/app-icon";
-import { Illustration } from "@/components/shared/illustration";
-import {AuthForm} from "@/features/auth/components";
-import { RoutesConfig } from "@/navigation";
 import { Link } from "react-router-dom";
+
+import { RoutesConfig } from "@/navigation";
+
+import { AuthForm } from "@/features/auth/components";
+import { AppIcon } from "@/components/shared/app-icon";
+
+import {ReactComponent as IllustrationImage} from '@/assets/media/illustration.svg';
 
 export function AuthenticationScreen() {
   return (
-    <div className={'grid flex-1 h-screen grid-cols-1 lg:grid-cols-12'}>
-      <div className={'flex-1 hidden relative lg:flex lg:col-span-5 bg-[#F5F6FA] border-r'}>
-        <div className={"m-10 absolute"}>
+    <div className={'flex flex-1 h-screen'}>
+      <div className={'flex-1 relative hidden isolate lg:flex max-w-lg bg-[#F5F6FA] border-r p-12'}>
         <Link to={RoutesConfig.home}>
           <AppIcon />
         </Link>
-        </div>
 
-        <Illustration />
+        <IllustrationImage className={"absolute inset-0 z-0 w-full h-full object-cover"} />
       </div>
-      <div className={'flex flex-1 lg:col-span-7 p-24 items-center justify-center'}>
+      <div className={'flex flex-1 p-24 items-center justify-center'}>
         <div className={'flex-1 max-w-[500px]'}>
           <AuthForm callbackRoute={''} />
         </div>
