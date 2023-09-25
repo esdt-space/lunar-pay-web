8faac20edd8396e6298be26f9d421431f63d040c
Alex <alex@devaccent.com>
2023-09-25 04:38:38 +0300

    Add react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,24 @@
 import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 
-import {MvxAxiosContext, MvxDappContext} from "@/lib/mvx";
+import { MvxAxiosContext, MvxDappContext } from "@/lib/mvx";
 import { ApplicationRouter } from "@/navigation";
+
+const queryClient = new QueryClient()
 
 function App() {
   return (
-    <MvxAxiosContext>
-      <BrowserRouter>
-          <MvxDappContext>
-            <ApplicationRouter />
-          </MvxDappContext>
-      </BrowserRouter>
-    </MvxAxiosContext>
+    <QueryClientProvider client={queryClient}>
+      <MvxAxiosContext>
+        <BrowserRouter>
+            <MvxDappContext>
+              <ApplicationRouter />
+            </MvxDappContext>
+        </BrowserRouter>
+      </MvxAxiosContext>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   )
 }
 
