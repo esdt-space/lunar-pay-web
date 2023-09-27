8f5dd6eac4a45921f581aa20931379dcd12a4bf0
unknown <davidbanciu57@gmail.com>
2023-09-27 17:47:54 +0300

    refactoring

--- src/screens/app/dashboard/components/assets.widget.tsx
+++ src/screens/app/dashboard/components/assets.widget.tsx
@@ -3,10 +3,10 @@ import { Card } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { EsdtToken, EsdtTokensList } from "@/features/tokens"
 import { transferTokenInteraction } from "@/features/vault/contract/interactions"
-import { cn, formatTokenBalance } from "@/theme/utils"
-import { checkIsValidAddress } from "@/utils"
+import { formatTokenBalance } from "@/theme/utils"
 import { Wallet } from "lucide-react"
 import { useState } from "react"
+import { AssetsInputAreaComponent } from "./assets-transfer-component"
 
 type Props = {
     assetSearchValue: string
@@ -37,8 +37,6 @@ export const DashboarAssetsWidget = (props: Props) => {
         transferTokenInteraction(selectedToken, Number(amount), address)
     }
 
-    const invalidAddressStyle = addresIsInvalid ? "border-red-500" : ""
-
     return <Card className={'flex flex-col flex-1 p-8 gap-4 shadow'}>
         <h2 className={'text-sm font-semibold uppercase tracking-wide'}>Assets</h2>
         <Input
@@ -67,56 +65,16 @@ export const DashboarAssetsWidget = (props: Props) => {
                     </div>
                 </div>
                 </div>
-            ))  : <div className={'flex flex-1 flex-col gap-4'}>
-                    <div className={'flex flex-1 flex-col gap-2'}>
-                        
-                        <div className='flex flex-1 justify-between items-center border rounded-md'>
-                            <div>
-                                <Input  
-                                    value={amount}
-                                    onChange={(e) => setAmount(e.target.value)}
-                                    style={{
-                                        boxShadow: 'none'
-                                    }}
-                                    className={'border-none focus:outline-none focus:ring-0 focus:border-none active:border-none hover:border-none placeholder:border-none disabled:border-none text-xs' }
-                                    type={"number"} 
-                                    placeholder='Enter Amount' />
-                            </div>
-
-                            <div className={'cursor-pointer mr-3'} onClick={getBalanceNumber}>
-                                <p className="font-extrabold">MAX</p>
-                            </div>
-                        </div>
-
-                        <Input  
-                            value={address}
-                            onChange={(e) => {
-                                if(!checkIsValidAddress(e.target.value)) {
-                                    setAddressIsInvalid(true)
-                                } else {
-                                    setAddressIsInvalid(false)
-                                }
-
-                                setAddress(e.target.value)
-                            }}
-                            style={{
-                                boxShadow: 'none'
-                            }}
-                            className={cn('focus:outline-none focus:ring-0 placeholder:border-none disabled:border-none text-xs', invalidAddressStyle)}
-                            type={"text"} 
-                            placeholder='Insert Address' />
-                        {addresIsInvalid && <p className={'text-red-500 text-xs ml-2 -mt-2'}>Address is Invalid</p>}
-                            
-                    </div>
-                    <div className={'flex flex-1 justify-between'}>
-                        <Button size={'sm'} variant={'outline'} onClick={() => setTriggerSendToken(false)} >
-                            Cancel
-                        </Button>
-                        <Button size={'sm'} variant={'outline'} onClick={sendToken} >
-                            Confirm
-                        </Button>
-                    </div>
-                </div>}
+            ))  : <AssetsInputAreaComponent 
+                    amount={amount}
+                    address={address}
+                    addresIsInvalid={addresIsInvalid}
+                    setAmount={setAmount}
+                    setAddress={setAddress}
+                    setAddressIsInvalid={setAddressIsInvalid}
+                    setTriggerSendToken={setTriggerSendToken}
+                    getBalanceNumber={getBalanceNumber}
+                    sendToken={sendToken} />}
         </div>
     </Card>
 }
