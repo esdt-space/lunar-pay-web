1cfd7591c729bcc602fae2cc5372a74262f0221d
Alex <alex@devaccent.com>
2023-09-28 04:47:13 +0300

    Add max width

--- src/components/layout/app-layout/index.tsx
+++ src/components/layout/app-layout/index.tsx
@@ -4,7 +4,7 @@ import {PublicPageHeader} from "./public-page-header.tsx";
 
 export function AppLayout() {
   return (
-    <main className={'flex flex-1 flex-col'}>
+    <main className={'flex flex-1 flex-col w-screen'}>
       <PublicPageHeader />
       <div className={'flex flex-1 mt-16'}>
         <Outlet />
