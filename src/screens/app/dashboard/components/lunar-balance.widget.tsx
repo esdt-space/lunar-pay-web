b8ec988c6249020c65b16d16269f50e497c5f6ae
unknown <davidbanciu57@gmail.com>
2023-09-28 23:35:21 +0300

    Displayed the total value of all tokens in USD

--- src/screens/app/dashboard/components/lunar-balance.widget.tsx
+++ src/screens/app/dashboard/components/lunar-balance.widget.tsx
@@ -1,12 +1,17 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 
-import { formatTokenBalance } from "@/theme/utils"
-import { useAccountEsdtTokensList } from "@/features/account-tokens/hooks";
+import { useAccountVaultTokens } from "@/features/vault/hooks";
 
 export const DashboardLunarBalanceWidget = () => {
-  //TODO: Need to get user's tokens from the vault
-  const vaultTokens = useAccountEsdtTokensList();
-  const lunarBalance = vaultTokens.reduce((acc, val) => acc + Number(formatTokenBalance(val.balance, val.decimals)) , 0)
+  const vaultTokens = useAccountVaultTokens();
+
+  let lunarBalance = vaultTokens.reduce((acc, val) => {
+    if(val.valueUsd !== undefined) {
+      acc += val.valueUsd
+    }
+
+    return acc
+  }, 0)
 
   return (
     <Card className={'flex-1 shadow p-2'}>
@@ -17,7 +22,7 @@ export const DashboardLunarBalanceWidget = () => {
       </CardHeader>
 
       <CardContent className={'space-y-3'}>
-        <div className={'text-2xl font-black'}>{lunarBalance}</div>
+        <div className={'text-2xl font-black'}>${lunarBalance}</div>
         <div className={'text-sm text-muted-foreground'}>
           Deposited balance is the overall sum of all the tokens available in the Lunar Pay Vault. This balance may
           not be accurate as balance rates are not available for all tokens.
