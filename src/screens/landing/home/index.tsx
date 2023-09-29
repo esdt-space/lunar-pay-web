871aa113b6cd61977705bc2a208fceee48bea19d
Alex <alex@devaccent.com>
2023-09-29 04:27:12 +0300

    Add badge to homepage

--- src/screens/landing/home/index.tsx
+++ src/screens/landing/home/index.tsx
@@ -2,6 +2,7 @@ import Spline from '@splinetool/react-spline';
 
 import { cn } from "@/theme/utils.ts";
 import { Button } from "@/components/ui/button.tsx";
+import {Badge} from "@/components/ui/badge.tsx";
 
 export function HomeScreen() {
   return (
@@ -11,6 +12,10 @@ export function HomeScreen() {
         "px-4 sm:px-12 xl:max-w-6xl xl:px-0"
       ])}>
           <div className="w-full max-w-[540px] lg:max-w-none lg:w-1/2">
+            <Badge variant={'outline'} className={'mb-2 p-2 px-4 text-primary font-semibold'}>
+              Live on MultiversX DEVNET
+            </Badge>
+
             <h2 className="text-4xl font-bold text-gray-800 max-w-[400px] leading-tight">
               <span className={'font-black'}>Payments</span> made simple for <span className={'font-black'}>WEB3 businesses</span>.
             </h2>
