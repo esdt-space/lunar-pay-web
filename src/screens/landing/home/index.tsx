faeb75b4e070f63d2780a17bb92495eab5317b8f
Alex <alex@devaccent.com>
2023-09-29 04:15:50 +0300

    Update homepage

--- src/screens/landing/home/index.tsx
+++ src/screens/landing/home/index.tsx
@@ -1,24 +1,34 @@
-import {Button} from "@/components/ui/button.tsx";
+import Spline from '@splinetool/react-spline';
+
+import { cn } from "@/theme/utils.ts";
+import { Button } from "@/components/ui/button.tsx";
 
 export function HomeScreen() {
   return (
-    <div className={'flex h-screen'}>
-      <div className="mx-auto px-4 sm:px-12 xl:max-w-6xl xl:px-0 mt-24">
-        <div className="space-y-6 md:flex lg:items-center lg:gap-6 lg:space-y-0">
-          <div className="md:7/12 lg:w-6/12">
-            <h2 className="text-3xl md:text-4xl xl:text-5xl font-bold text-gray-800">Payments made simple for WEB3 businesses.</h2>
-            <p className="mt-8 text-gray-600">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum omnis voluptatem accusantium nemo perspiciatis delectus atque autem! repellat expedita consequatur! Officiis id consequatur atque doloremque!</p>
+    <div className={'flex min-h-screen'}>
+      <div className={cn([
+        "isolate flex lg:mx-auto mt-24 gap-12 items-center",
+        "px-4 sm:px-12 xl:max-w-6xl xl:px-0"
+      ])}>
+          <div className="w-full max-w-[540px] lg:max-w-none lg:w-1/2">
+            <h2 className="text-4xl font-bold text-gray-800 max-w-[400px] leading-tight">
+              <span className={'font-black'}>Payments</span> made simple for <span className={'font-black'}>WEB3 businesses</span>.
+            </h2>
+            <p className="mt-8 text-muted-foreground">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum omnis voluptatem accusantium nemo perspiciatis delectus atque autem! repellat expedita consequatur! Officiis id consequatur atque doloremque!
+            </p>
             <p className="mb-12 mt-4 text-gray-600">Nobis minus voluptatibus pariatur dignissimos libero quaerat iure expedita at? Asperiores nemo possimus nesciunt dicta veniam aspernatur quam mollitia.</p>
 
             <Button size={'lg'}>GET STARTED</Button>
           </div>
 
-          <div className="md:5/12 lg:w-6/12">
-            <img className="md:-ml-8" src="./images/some-image.webp" alt="" loading="lazy" />
+          <div className={cn([
+            "-right-[25%] -top-[10%] md:-right-6 sm:top-auto",
+            "absolute lg:static md:w-5/12 lg:w-1/2 -z-10 pointer-events-none opacity-50 sm:opacity-100",
+          ])}>
+            <Spline className='w-full' scene="https://prod.spline.design/jarUpvCPzoBGEUIe/scene.splinecode"/>
           </div>
-        </div>
       </div>
-
     </div>
   )
 }
