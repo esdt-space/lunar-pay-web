73e6a45cab1669f98443334e6fc15f28d2ed41b7
Alex <alex@devaccent.com>
2023-10-01 06:16:28 +0300

    Update homepage

--- src/screens/landing/home/index.tsx
+++ src/screens/landing/home/index.tsx
@@ -1,44 +1,55 @@
+import { Link } from "react-router-dom";
 import Spline from '@splinetool/react-spline';
 
 import { cn } from "@/theme/utils.ts";
-import { Button } from "@/components/ui/button.tsx";
+import { RoutesConfig } from "@/navigation";
+
 import { Badge } from "@/components/ui/badge.tsx";
 
+import { WhyUsPartial } from "./partials/why-us-partial.tsx";
 import { HomeFeaturesPartial } from "./partials/features.tsx";
+import { StartTodayPartial } from "./partials/start-today-partial.tsx";
 
 export function HomeScreen() {
   return (
-    <div className={'flex flex-col gap-48 pb-48'}>
+    <div className={'flex flex-col gap-24 lg:gap-48 pb-24 lg:pb-48'}>
       <div className={cn([
-        "container flex flex-1 isolate lg:mx-auto mt-48 lg:mt-40 gap-12 items-center",
+        "relative container flex flex-1 isolate lg:mx-auto mt-48 lg:mt-40 gap-12 items-center",
       ])}>
-          <div className="w-full max-w-[540px] lg:max-w-none lg:w-1/2">
-            <Badge variant={'outline'} className={'mb-2 p-2 px-4 text-primary font-semibold'}>
+          <div className="flex flex-col w-full max-w-[540px] lg:max-w-none lg:w-1/2">
+            <Badge variant={'outline'} className={'mb-2 py-1.5 px-4 font-bold self-start text-gray-600'}>
               Live on MultiversX DEVNET
             </Badge>
 
             <h2 className="text-4xl font-bold text-gray-800 max-w-[400px] leading-tight">
               <span className={'font-black'}>Payments</span> made simple for <span className={'font-black'}>WEB3 businesses</span>.
             </h2>
-            <p className="mt-8 text-gray-600">
+            <p className="mt-8 text-gray-700">
               Lunar Pay is reshaping the financial landscape of WEB3. We offer individuals, businesses, investors, and teams enhanced flexibility and streamlined access to tokens.
             </p>
-            <p className="mb-12 mt-4 text-gray-600">
+            <p className="mb-12 mt-4 text-gray-700">
               Introducing the Vault — a groundbreaking smart contract designed to act as your personalized funding wallet. With the Vault, you can now establish and manage payment agreements tailored to your needs, setting the stage for a multitude of financial utilities in the WEB3 space.
             </p>
 
-            <Button size={'lg'}>GET STARTED</Button>
+            <Link
+              className={'rounded-full bg-gradient-to-br from-primary to-secondary py-3 px-8 self-start'}
+              to={RoutesConfig.dashboard}
+            >
+              <span className={'text-sm text-white font-semibold'}>Launch App</span>
+            </Link>
           </div>
 
           <div className={cn([
-            "-right-[25%] -top-[10%] md:-right-6 sm:top-auto",
+            "-right-[220px] -top-[340px] md:-right-6 sm:top-auto",
             "absolute lg:static md:w-5/12 lg:w-1/2 -z-10 pointer-events-none opacity-50 sm:opacity-100",
           ])}>
             <Spline className='w-full' scene="https://prod.spline.design/jarUpvCPzoBGEUIe/scene.splinecode"/>
           </div>
       </div>
 
+      <WhyUsPartial />
       <HomeFeaturesPartial />
+      <StartTodayPartial />
     </div>
   )
 }
---
+++ src/screens/landing/home/partials/why-us-partial.tsx
@@ -0,0 +1,66 @@
+import { ReactNode} from "react";
+import {PencilRuler, Shield, Wallet2} from "lucide-react";
+
+import { cn } from "@/theme/utils.ts";
+
+type WhyUsItemProps = {
+  title: string;
+  description: string;
+  icon: ReactNode;
+}
+
+function WhyUsItem(props: WhyUsItemProps) {
+  return (
+    <li className={cn([
+      "col-span-full lg:col-span-1",
+      "flex flex-col gap-2",
+    ])}>
+      <div className={'p-2.5 bg-white bg-opacity-5 rounded self-start'}>{props.icon}</div>
+      <h3 className="text-md leading-none font-bold">{props.title}</h3>
+      <p className="mt-1 text-sm leading-normal text-slate-300/80">{props.description}</p>
+    </li>
+  )
+}
+
+export function WhyUsPartial() {
+  return (
+    <section className={cn([
+      "container bg-blue-950 text-white",
+      "2xl:rounded-2xl box-border"
+    ])}>
+      <div className="py-20 lg:px-6">
+        <div>
+          <div className={'flex gap-2 items-center'}>
+            <div className={'w-2 bg-gradient-to-bl from-primary to-secondary rounded'}>&nbsp;</div>
+            <h4 className="text-xl font-semibold leading-none text-slate-300">Why Choose Lunar Pay?</h4>
+          </div>
+
+          <div className="text-3xl text-white font-semibold tracking-tight max-w-xl mt-3">
+            <span>Elevate your financial interactions</span>
+            <span className={'opacity-30'}> by intertwining simplicity with sophistication.</span>
+          </div>
+        </div>
+
+        <ul className="grid grid-cols-3 gap-12 mt-16">
+          <WhyUsItem
+            icon={<Wallet2 className={'w-5 h-5'} />}
+            title={'Revolutionize Your Payments'}
+            description={'Dive deep into efficient transaction management with the Vault, our state-of-the-art smart contract. Lunar Pay offers a direct interface, enabling you to craft and oversee diverse payment structures with precision and ease, streamlining your financial operations for optimal results.'}
+          />
+
+          <WhyUsItem
+            icon={<Shield className={'w-5 h-5'} />}
+            title={'Secure and Transparent'}
+            description={'Navigate with confidence in a transparent and secure environment, cultivated by Lunar Pay’s adherence to the principles of decentralization. We expose the core of our secure transaction management to you, ensuring integrity and trust in every interaction within the decentralized network.'}
+          />
+
+          <WhyUsItem
+            icon={<PencilRuler className={'w-5 h-5'} />}
+            title={'Tailored to Your Needs'}
+            description={'In a world where your financial needs evolve, Lunar Pay adapts, providing a versatile financial ecosystem that facilitates managing various financial avenues, from subscriptions and payroll to team allowances, all unified and streamlined for your convenience and strategic financial planning.'}
+          />
+        </ul>
+      </div>
+    </section>
+  )
+}
