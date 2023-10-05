58cab1684a99efe2d21cd7c53cc2125d61a18dd6
unknown <davidbanciu57@gmail.com>
2023-10-05 21:10:11 +0300

    Added frequency select input

--- src/screens/app/subscriptions/components/payment-details.widget.tsx
+++ src/screens/app/subscriptions/components/payment-details.widget.tsx
@@ -6,16 +6,21 @@ import { useTokensList } from "@/features/tokens/hooks/use-tokens";
 import { useState } from "react";
 import { ScreenTabs } from "../agreement.screen";
 import { AgreementsService } from "@/features/subscription/subscriptions.service";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { cn } from "@/theme/utils";
 
 type Props = {
   setSelectedTab: React.Dispatch<React.SetStateAction<ScreenTabs>>
 }
+
+const frequencyList = ["SS", "MM", "HH", "D", "W", "M", "Y"]
 
 export const PaymentDetailsWidget = ({setSelectedTab}: Props) => {
   const [selectedToken, setSelectedToken] = useState<EsdtToken | undefined>(undefined);
   // const [paymentType, setPaymentType] = useState(PaymentType.FixedAmount)
   // const [address, setAddress] = useState('')
   const [amount, setAmount] = useState('')
+  const [frequency, setFrequency] = useState('')
   const [isMandatoryField, setIsMandatoryField] = useState(false)
 
   const tokens = useTokensList();
@@ -32,8 +37,9 @@ export const PaymentDetailsWidget = ({setSelectedTab}: Props) => {
     }
 
     const input = {
+      token: selectedToken,
       amount: amount,
-      token: selectedToken
+      frequency: frequency
     }
 
     return AgreementsService.createAgreement(input).then(() => setSelectedTab(ScreenTabs.AgreementDetails))
@@ -65,14 +71,30 @@ export const PaymentDetailsWidget = ({setSelectedTab}: Props) => {
       onChange={(token) => setSelectedToken(token)}
     />
     <div>
-      <Input 
-        placeholder="Insert amount"
-        className={amountInputStyle}
-        value={amount}
-        onChange={(e) => {
-          setAmount(e.target.value)
-          setIsMandatoryField(false)
-        }} />
+      <div className="flex">
+        <Input 
+          placeholder="Insert amount"
+          className={cn(["w-9/12", amountInputStyle])}
+          value={amount}
+          onChange={(e) => {
+            setAmount(e.target.value)
+            setIsMandatoryField(false)
+          }} />
+        <div className="w-3/12">
+          <Select onValueChange={(item) => setFrequency(item)} defaultValue={"Frequency"}>
+            <SelectTrigger id="framework">
+              <SelectValue placeholder="Frequency" />
+            </SelectTrigger>
+            <SelectContent position="popper">
+              {frequencyList.map((item, index) => {
+                return <div key={index}>
+                  <SelectItem value={item}>/{item}</SelectItem>
+                </div>
+              })}
+            </SelectContent>
+          </Select>
+        </div>
+      </div>
       {missingAmount && <p className='text-red-500 text-xs ml-2'>Mandatory Field</p>}
     </div>
     <div className="flex w-full">
