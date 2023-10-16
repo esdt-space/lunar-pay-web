fff0e0e085259360a658022f90d9e9dc4cb61727
Alex <alex@devaccent.com>
2023-10-16 04:23:49 +0300

    Fix app build

--- src/lib/mvx/transactions/send-transactions.handler.ts
+++ src/lib/mvx/transactions/send-transactions.handler.ts
@@ -1,7 +1,6 @@
+import { Transaction } from "@multiversx/sdk-core/out";
+import { refreshAccount } from '@multiversx/sdk-dapp/utils'
 import { sendTransactions } from '@multiversx/sdk-dapp/services'
-import { Transaction, TransactionWatcher } from "@multiversx/sdk-core/out";
-import { ApiNetworkProvider } from "@multiversx/sdk-network-providers/out";
-import { getNetworkConfig, refreshAccount } from '@multiversx/sdk-dapp/utils'
 import type { SendTransactionsPropsType, TransactionsDisplayInfoType } from '@multiversx/sdk-dapp/types'
 
 export async function sendTransactionsHandler(
