e77c9cb586ea24884aa9a2c35598d143a2d6e3c9
BDavid57 <davidbanciu57@gmail.com>
2023-10-17 20:14:31 +0300

    Used mock data to display some transactions on the transactions screen

--- src/features/transactions/components/transactions-table/table.tsx
+++ src/features/transactions/components/transactions-table/table.tsx
@@ -1,29 +1,30 @@
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table"
 import { TransactionItem } from "../../models"
+import moment from "moment/moment";
 
 type Props = {
   transactions: TransactionItem[];
 }
 
-export const Transactionstable = ({transactions}: Props) => {
+export const AgreementsTransactionsTable = ({transactions}: Props) => {
   return <Table>
     <TableHeader>
         <TableRow>
+          <TableHead />
           <TableHead>Address</TableHead>
-          <TableHead>Amount</TableHead>
           <TableHead>Token</TableHead>
-          <TableHead>Status</TableHead>
+          <TableHead>Amount</TableHead>
           <TableHead className={'max-lg:hidden'}>Date</TableHead>
         </TableRow>
       </TableHeader>
       <TableBody>
         {transactions.map((item, index) => (
           <TableRow key={index}>
-            <TableCell>{item.address}</TableCell>
-            <TableCell>{item.amount}</TableCell>
-            <TableCell>{item.token}</TableCell>
             <TableCell>{item.status}</TableCell>
-            <TableCell>{item.date}</TableCell>
+            <TableCell>{item.address}</TableCell>
+            <TableCell>{item.token}</TableCell>
+            <TableCell>{item.amount}</TableCell>
+            <TableCell>{moment(item.date).format('ll')}</TableCell>
           </TableRow>
         ))}
       </TableBody>
--- src/screens/app/transactions/transactions.screen.tsx
+++ src/screens/app/transactions/transactions.screen.tsx
@@ -1,5 +1,19 @@
+import { Card, CardContent } from "@/components/ui/card"
+import { AgreementsTransactionsTable } from "@/features/transactions"
+import { transactionsMockData } from "./transactions-mock.data"
+import { PaginationButtons, usePagination } from "@/components/shared/pagination";
+
 export const TransactionsScreen = () => {
+
+  const { data: paginatedTransactions, ...rest} =
+    usePagination(transactionsMockData, 5);
+    
   return <div className={'container mx-auto p-4 sm:p-12 xl:p-16'}>
-    Transactions Screen
+    <Card>
+      <CardContent>
+        <AgreementsTransactionsTable transactions={paginatedTransactions} />
+        <PaginationButtons {...{...rest}} />
+      </CardContent>
+    </Card>
   </div>
 }
