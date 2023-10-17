fb93eda959315268c67677a531ee781521f6a79e
Alex <alex@devaccent.com>
2023-10-17 08:12:59 +0300

    Update screens and fix build

--- src/screens/landing/donations/partials/features-partial.tsx
+++ src/screens/landing/donations/partials/features-partial.tsx
@@ -1,3 +1,5 @@
+import {cn} from "@/theme/utils.ts";
+
 type ItemProps = {
   title: string;
   description: string;
@@ -5,10 +7,10 @@ type ItemProps = {
 
 function Item(props: ItemProps) {
   return (
-    <li className="relative flex items-top gap-3">
+    <li className="relative bg-white bg-opacity-90 p-8 rounded-md ring-1 ring-slate-200">
       <div
-        className="relative bg-gradient-to-br from-primary to-secondary w-4 h-4 top-1.5 opacity-80">&nbsp;</div>
-      <div className={'flex-1'}>
+        className="relative bg-gradient-to-br from-primary to-secondary w-4 h-4 mb-3 opacity-80">&nbsp;</div>
+      <div className={'flex-1 space-y-1'}>
         <h2 className="text-xl font-semibold text-gray-900 text-left">{props.title}</h2>
         <p className="text-gray-700 text-left max-w-md">{props.description}</p>
       </div>
@@ -18,8 +20,14 @@ function Item(props: ItemProps) {
 
 export function FeaturesPartial() {
   return (
-    <div className="bg-gray-100 ">
-      <div className="container mx-auto p-10 pb-40">
+    <div className={cn([
+      'relative flex py-8 isolate',
+      'before:absolute before:inset-0',
+      `before:z-[2] before:shadow-[rgb(249,250,251)_0px_150px_200px_inset,rgb(249,250,251)_0px_-200px_200px_inset]`,
+      `after:absolute after:inset-0 after:z-[1] after:bg-gradient-to-br after:from-primary/20 after:to-secondary/20`,
+      `after:[mask-image:url('/assets/media/patterns/waves-pattern.svg')] after:[mask-size:64px_32px] after:[mask-repeat:repeat]`,
+    ])}>
+      <div className="container mx-auto p-10 pb-40 z-[3]">
         <section className="text-center">
           <h2 className="text-4xl font-bold text-gray-900 mb-4 pt-10">
             Features
--- src/screens/landing/payroll/index.tsx
+++ src/screens/landing/payroll/index.tsx
@@ -1,21 +1,20 @@
 import { JoinLunarPayRevolution } from "@/components/shared/sections";
 
 import { HeroPartial } from "./partials/hero-partial.tsx";
-import { FeaturesPartial} from "./partials/features-partial.tsx";
+import { FeaturesPartial } from "./partials/features-partial.tsx";
 import { WhyUsPartial } from "./partials/why-us-partial.tsx";
 
-export function Payroll(){
+export function Payroll() {
+  return (
+    <div className={'payroll-page flex flex-col gap-24 lg:gap-48 pb-24 lg:pb-48 bg-gray-50/50'}>
+      <HeroPartial/>
+      <FeaturesPartial/>
+      <WhyUsPartial/>
 
-    return (
-        <div className={'payroll-page flex flex-col gap-24 lg:gap-48 pb-24 lg:pb-48'}>
-            <HeroPartial />
-            <FeaturesPartial />
-            <WhyUsPartial />
-
-            <JoinLunarPayRevolution
-              subtitle={'Looking to modernize your payroll system?'}
-              description={'Experience the advantages of Lunar Pay Payroll. Combining user-centric design, consistent payment assurance, and adaptability tailored to your specific demands, we\'re committed to driving your success.'}
-            />
-        </div>
-    );
+      <JoinLunarPayRevolution
+        subtitle={'Looking to modernize your payroll system?'}
+        description={'Experience the advantages of Lunar Pay Payroll. Combining user-centric design, consistent payment assurance, and adaptability tailored to your specific demands, we\'re committed to driving your success.'}
+      />
+    </div>
+  );
 }
