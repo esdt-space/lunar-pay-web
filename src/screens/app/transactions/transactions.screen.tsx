f6322659e8b2e00698e71efe079e05d55fb7a389
BDavid57 <davidbanciu57@gmail.com>
2023-10-18 17:10:35 +0300

    Added filtering by address to the transactions list

--- src/screens/app/transactions/transactions.screen.tsx
+++ src/screens/app/transactions/transactions.screen.tsx
@@ -2,13 +2,32 @@ import { Card, CardContent } from "@/components/ui/card"
 import { AgreementsTransactionsTable } from "@/features/transactions"
 import { transactionsMockData } from "./transactions-mock.data"
 import { PaginationButtons, usePagination } from "@/components/shared/pagination";
+import { useMemo, useState } from "react";
+import { Input } from "@/components/ui/input";
 
 export const TransactionsScreen = () => {
+  const [filterValue, setFilterValue] = useState("")
+
+  const filteredTransactions = useMemo(() => {
+    if(filterValue === "") return transactionsMockData;
+    const lowercaseFilterValue = filterValue.toLocaleLowerCase();
+
+    return transactionsMockData.filter(item =>
+      item.address.toLocaleLowerCase().includes(lowercaseFilterValue)
+    );
+  }, [transactionsMockData, filterValue]);
 
   const { data: paginatedTransactions, ...rest} =
-    usePagination(transactionsMockData, 5);
+    usePagination(filteredTransactions, 5);
     
   return <div className={'container mx-auto p-4 sm:p-12 xl:p-16'}>
+    <Input
+      value={filterValue}
+      onChange={e => setFilterValue(e.target.value)}
+      placeholder={'Filter ...'}
+      className={'justify-self-end max-w-sm mb-4'}
+    />
+
     <Card>
       <CardContent>
         <AgreementsTransactionsTable transactions={paginatedTransactions} />
