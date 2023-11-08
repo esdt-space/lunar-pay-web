98915a02969fb7a43ea3bc8c8915e288f1202ea7
Alex <alex@devaccent.com>
2023-11-08 15:43:01 +0200

    Fix frequency

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,77 +1,57 @@
+const secondsInMinute = 60;
+const secondsInHour = secondsInMinute * 60;
+const secondsInDay = secondsInHour * 24;
+const secondsInWeek = secondsInDay * 7;
+const secondsInMonth = secondsInDay * 30;
+const secondsInYear = secondsInDay * 365;
+
 export const getPaymentFrequency = (input: string) => {
   switch(input) {
-    case('Per Minute'): {
-      return 60
-    }
-    case('Per Hour'): {
-      return 3600
-    }
-    case('Daily'): {
-      return 3600 * 24
-    }
-    case('Weekly'): {
-      return 3600 * 24 * 7
-    }
-    case('Monthly'): {
-      return 3600 * 24 * 7 * 30
-    }
-    case('Per Year'): {
-      return 3600 * 24 * 7 * 30 * 12
-    }
-    default: {
-      return 60
-    }
+    case 'Per Minute':
+      return secondsInMinute
+    case 'Per Hour':
+      return secondsInHour
+    case 'Daily':
+      return secondsInDay
+    case 'Weekly':
+      return secondsInWeek
+    case 'Monthly':
+      return secondsInMonth
+    case 'Per Year':
+      return secondsInYear
   }
 }
 
 export const formatFrequency = (input: number) => {
   switch(input) {
-    case(60): {
-      return 'Per Minute'
-    }
-    case(3600): {
-      return 'Per Hour'
-    }
-    case(3600 * 24): {
-      return 'Daily'
-    }
-    case(3600 * 24 * 7): {
-      return 'Weekly'
-    }
-    case(3600 * 24 * 7 * 30): {
-      return 'Monthly'
-    }
-    case(3600 * 24 * 7 * 30 * 12): {
-      return 'Per Year'
-    }
-    default: {
-      return "Per Minute"
-    }
+    case secondsInMinute:
+      return 'Per Minute';
+    case secondsInHour:
+      return 'Per Hour';
+    case secondsInDay:
+      return 'Daily';
+    case secondsInWeek:
+      return 'Weekly';
+    case secondsInMonth:
+      return 'Monthly';
+    case secondsInYear:
+      return 'Per Year';
   }
 }
 
 export const formatFrequencyForSignAgreement = (input: number) => {
   switch(input) {
-    case(60): {
-      return 'minute'
-    }
-    case(3600): {
+    case secondsInMinute:
+      return 'minute';
+    case secondsInHour:
       return 'hour'
-    }
-    case(3600 * 24): {
-      return 'day'
-    }
-    case(3600 * 24 * 7): {
-      return 'week'
-    }
-    case(3600 * 24 * 7 * 30): {
-      return  'month'
-    }
-    case(3600 * 24 * 7 * 30 * 12): {
-      return 'year'
-    }
-    default: {
-      return "minute"
-    }
+    case secondsInDay:
+      return 'day';
+    case secondsInWeek:
+      return 'week';
+    case secondsInMonth:
+      return  'month';
+    case secondsInYear:
+      return 'year';
   }
 }
