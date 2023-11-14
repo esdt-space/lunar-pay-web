ea47bb1e40743b9d196b508521d9e70a51350015
David Banciu <davidbanciu57@gmail.com>
2023-11-14 16:57:05 +0000

    Added binance button in the withdraw widget

--- src/components/prefab/binance-pay-button.tsx
+++ src/components/prefab/binance-pay-button.tsx
@@ -1,14 +1,26 @@
 import BinanceLogo from '@/assets/third-party/binance-logo.svg?react';
 import { Button } from "@/components/ui/button.tsx";
 
-export function BinancePayButton() {
+type Props = {
+  disableButton?: boolean,
+  buttonFn?: () => void
+}
+
+export function BinancePayButton(props: Props) {
+  const { disableButton, buttonFn } = props;
+
   return (
-    <Button disabled size={'sm'} className={'px-4 text-sm font-bold'}>
+    <Button 
+      disabled={disableButton} 
+      onClick={buttonFn}
+      size={'sm'} 
+      className={'px-4 text-sm font-bold'}
+    >
       <BinanceLogo className={'w-4 h-4 mr-2 !fill-[#F3BA2F]'} />
       <div className={'flex items-center gap-1'}>
         <div className={'text-black'}>BINANCE</div>
         <div className={'text-[#F3BA2F]'}>PAY</div>
-        <div className={'text-xs font-normal opacity-60'}>(coming soon)</div>
+        {disableButton && <div className={'text-xs font-normal opacity-60'}>(coming soon)</div>}
       </div>
     </Button>
   );
--- src/screens/app/dashboard/components/withdraw-component.tsx
+++ src/screens/app/dashboard/components/withdraw-component.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { Minus } from "lucide-react"
 
 import { Button } from "@/components/ui/button"
+import { Tabs, TabsContent } from "@/components/ui/tabs";
 
 import { EsdtToken } from "@/features/tokens";
 import { TokenSelectorWithAmount } from "@/features/tokens/components"
@@ -9,10 +10,18 @@ import { getTokenErrorForValue } from "@/features/tokens/validation";
 import { useAccountVaultTokens } from "@/features/vault/hooks";
 
 import { useWithdrawEgldMutation, useWithdrawEsdtMutation } from "@/features/vault/hooks/mutations";
+import { BinancePayButton } from "@/components/prefab/binance-pay-button";
+import { TransferAssetComponent } from "./transfer-asset-component";
+
+enum ScreenTabs {
+  Withdraw = 'withdraw',
+  BinanceTransfer = 'binance-transfer'
+}
 
 export const WithdrawAssetsComponent = () => {
   const [amount, setAmount] = useState('');
   const [selectedToken, setSelectedToken] = useState<EsdtToken | undefined>(undefined);
+  const [selectedTab, setSelectedTab] = useState<ScreenTabs>(ScreenTabs.Withdraw)
 
   useEffect(() => {
     setAmount("");
@@ -35,20 +44,44 @@ export const WithdrawAssetsComponent = () => {
     return withdrawEgldHandler(Number(amount))
   }
 
-  return <div className="flex flex-1 flex-col gap-4">
-    <TokenSelectorWithAmount
-      token={selectedToken}
-      tokens={vaultTokens}
-      onTokenChange={(token) => setSelectedToken(token)}
-      amount={amount}
-      onAmountChange={(amount) => setAmount(amount)}
-    />
-    <div className={'text-sm text-muted-foreground'}>
-      Withdraw assets from the Lunar Pay Vault.
-    </div>
-    <Button size={'sm'} onClick={withdrawToken} disabled={!canPerformOperation}>
-      Withdraw
-      <Minus className={'ml-1 w-4 h-4'} />
-    </Button>
+  const openBinanceTransfer = () => {
+    if ( selectedToken === undefined ) return
+
+    setSelectedTab(ScreenTabs.BinanceTransfer)
+  }
+
+  return <div>
+    <Tabs value={selectedTab} className="flex flex-col space-y-0">
+      <TabsContent className={'space-y-3 mt-0'} value={ScreenTabs.Withdraw}>
+        <div className="flex flex-1 flex-col gap-4">
+          <TokenSelectorWithAmount
+            token={selectedToken}
+            tokens={vaultTokens}
+            onTokenChange={(token) => setSelectedToken(token)}
+            amount={amount}
+            onAmountChange={(amount) => setAmount(amount)}
+          />
+          <div className={'text-sm text-muted-foreground'}>
+            Withdraw assets from the Lunar Pay Vault.
+          </div>
+
+          <div className={'flex flex-1 flex-col gap-2'}>
+            <Button size={'sm'} onClick={withdrawToken} disabled={!canPerformOperation}>
+              Withdraw
+              <Minus className={'ml-1 w-4 h-4'} />
+            </Button>
+
+            <BinancePayButton buttonFn={openBinanceTransfer} />
+          </div>
+        </div>
+      </TabsContent>
+
+      <TabsContent className={'mt-0'} value={ScreenTabs.BinanceTransfer}>
+        <TransferAssetComponent
+          selectedToken={selectedToken as EsdtToken}
+          finishCallback={() => setSelectedTab(ScreenTabs.Withdraw)}
+        />
+      </TabsContent>
+    </Tabs>
   </div>
 }
