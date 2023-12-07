a351779a027354841da76920a0df9a8d2750d94f
David Banciu <davidbanciu57@gmail.com>
2023-12-07 17:56:42 +0000

    Added styling for contact screen

--- src/screens/app/contact/contact.screen.tsx
+++ src/screens/app/contact/contact.screen.tsx
@@ -2,25 +2,29 @@ import { ContainedScreen } from "@/components/prefab/contained-screen"
 import { Card } from "@/components/ui/card"
 import { TelegramLogo, TwitterLogo } from "./icons"
 import { ContactForm } from "./contact-form"
+import { Separator } from "@/components/ui/separator.tsx"
 
 export const ContactScreen = () => {
   return (
     <ContainedScreen>
       <Card className="flex justify-between p-6">
-        <div>
+        <div className="w-5/12">
           <ContactForm/>
         </div>
-        <div className={'space-y-8'}>
+        <div><Separator orientation="vertical"/></div>
+        <div className="w-5/12">
           <div>
-            <h1>Contact us</h1>
-            <p>Send us an email or give us a call, we try to answer all enquiries within 24 hours on business days.</p>
-            <p>E: this@something.ro</p>
-            <p>P: number</p>
+            <h2 className={'text-3xl font-medium mb-4'}>Contact us</h2>
+            <p className="mb-2">Send us an email or give us a call, we try to answer<br />all enquiries within 24 hours on business days.
+            </p>
+            <p>Email: this@something.ro</p>
+            <p>Phone: number</p>
           </div>
+          <div className="mt-4 mb-4"><Separator/></div>
           <div>
-            <h2>Social Media</h2>
-            <TelegramLogo>text</TelegramLogo>
-            <TwitterLogo/>
+            <h2 className={'text-3xl font-medium mb-4'}>Social Media</h2>
+            <TelegramLogo>Telegram</TelegramLogo>
+            <TwitterLogo>Twitter</TwitterLogo>
           </div>
         </div>
       </Card >
--- src/screens/app/contact/icons/twitter.logo.tsx
+++ src/screens/app/contact/icons/twitter.logo.tsx
@@ -3,9 +3,9 @@ import logoLoader from "./twitter-logo.svg";
 
 export function TwitterLogo(props: PropsWithChildren) {
   return (
-    <div>
-      <img src={logoLoader}/>
-      <div className={'text-center text-sm text-muted-foreground mt-4 max-w-prose'}>
+    <div className="flex direction-row mb-2 mt-2 cursor-pointer">
+      <img src={logoLoader} className="w-7 h-7 mr-2"/>
+      <div className={''}>
         {props.children}
       </div>
     </div>
