b50252d66aafd1111e436e5297a6d2da33a87cc0
David Banciu <davidbanciu57@gmail.com>
2023-12-07 18:03:10 +0000

    Added redirect links to social media icons

--- src/screens/app/contact/icons/telegram.logo.tsx
+++ src/screens/app/contact/icons/telegram.logo.tsx
@@ -2,8 +2,15 @@ import { PropsWithChildren } from "react";
 import logoLoader from "./telegram-logo.svg";
 
 export function TelegramLogo(props: PropsWithChildren) {
+
+  const telegramUrl = "https://web.telegram.org/a/#-1002098653927"
+
+  const redirect = () => {
+    window.location.href = telegramUrl
+  }
+
   return (
-    <div className="flex direction-row mb-2 mt-2 cursor-pointer">
+    <div onClick={redirect} className="flex direction-row mb-2 mt-2 cursor-pointer">
       <img src={logoLoader} className="w-7 h-7 mr-2"/>
       <div className={''}>
         {props.children}
--- src/screens/app/contact/icons/twitter.logo.tsx
+++ src/screens/app/contact/icons/twitter.logo.tsx
@@ -2,8 +2,15 @@ import { PropsWithChildren } from "react";
 import logoLoader from "./twitter-logo.svg";
 
 export function TwitterLogo(props: PropsWithChildren) {
+
+  const twitterUrl = "https://twitter.com/pay_lunar"
+
+  const redirect = () => {
+    window.location.href = twitterUrl
+  }
+
   return (
-    <div className="flex direction-row mb-2 mt-2 cursor-pointer">
+    <div onClick={redirect} className="flex direction-row mb-2 mt-2 cursor-pointer">
       <img src={logoLoader} className="w-7 h-7 mr-2"/>
       <div className={''}>
         {props.children}
