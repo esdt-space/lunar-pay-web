5517083c8192848b08889d212570593f3474df35
David Banciu <davidbanciu57@gmail.com>
2023-12-08 18:49:34 +0000

    Created reusable component for questions list

--- src/screens/app/static-pages/frequently-asked-questions/faq.component.tsx
+++ src/screens/app/static-pages/frequently-asked-questions/faq.component.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { ListItem } from "./components/list-item";
 
 export const FrequentlyAskedQuestions: React.FC = () => {
   
@@ -6,50 +7,50 @@ export const FrequentlyAskedQuestions: React.FC = () => {
     <div className="max-w-4xl mx-auto p-4">
       <h1 className="text-2xl font-bold mb-12 text-center">Frequently Asked Questions</h1>
       <ul>
-        <li>
-          <h2 className="font-semibold">What is Lunar Pay?</h2>
-          <p className="mb-4">Lunar Pay is a revolutionary financial platform on the MultiversX blockchain, designed to simplify and enhance financial operations in the Web3 ecosystem. It offers a smart contract-based wallet for managing financial transactions with enhanced flexibility and control.</p>
-        </li>
-        <li>
-          <h2 className="font-semibold">What makes Lunar Pay secure?</h2>
-          <p className="mb-4">Lunar Pay prioritizes security by being non-custodial and permissionless. This means users have full control over their funds without needing extra permission to use the platform. Additionally, regular security audits and compliance with blockchain regulations enhance the platform's safety.</p>
-        </li>
-        <li>
-          <h2 className="font-semibold">Can Lunar Pay automate payroll processes?</h2>
-          <p className="mb-4">Absolutely. Lunar Pay uses the MultiversX blockchain to automate payroll, enabling businesses to efficiently distribute salaries and bonuses while ensuring security and compliance.</p>
-        </li>
-        <li>
-          <h2 className="font-semibold">Does Lunar Pay support subscription management?</h2>
-          <p className="mb-4">Yes, Lunar Pay offers a crypto subscription solution for businesses, streamlining the process of generating subscription revenue and maximizing customer retention.</p>
-        </li>
-        <li>
-          <h2 className="font-semibold">How can Lunar Pay assist in portfolio management?</h2>
-          <p className="mb-4">Lunar Pay facilitates efficient token transfers to multiple recipients and provides users with insights into their expenditures and anticipated earnings, aiding in effective portfolio management.</p>
-        </li>
-        <li>
-          <h2 className="font-semibold">What is Lunar Pay Donations?</h2>
-          <p className="mb-4">Lunar Pay Donations is a feature designed for content creators, nonprofit organizations, and individuals to manage and accept recurring donations seamlessly on the MultiversX blockchain.</p>
-        </li>
-        <li>
-          <h2 className="font-semibold">Who can benefit from using Lunar Pay?</h2>
-          <p className="mb-4">Lunar Pay is tailored for individuals, businesses, content creators, and nonprofit organizations looking for an efficient, secure, and user-friendly platform for managing financial transactions in the Web3 space.</p>
-        </li>
-        <li>
-          <h2 className="font-semibold">Is Lunar Pay compliant with financial regulations?</h2>
-          <p className="mb-4">Yes, Lunar Pay is developed with a strong focus on compliance. We continually monitor regulatory changes and ensure that our platform adheres to the relevant laws and regulations in the blockchain and cryptocurrency sectors.</p>
-        </li>
-        <li>
-          <h2 className="font-semibold">How can I start using Lunar Pay?</h2>
-          <p className="mb-4">Getting started with Lunar Pay is simple. Just sign up on our website, and you can begin managing your financial transactions immediately.</p>
-        </li>
-        <li>
-          <h2 className="font-semibold">Are there any fees associated with using Lunar Pay?</h2>
-          <p className="mb-4">Lunar Pay charges a transaction fee of 0.5% per transaction. This fee structure is designed to sustain our platform while keeping it accessible to a wide range of users. Besides this, there are the MultiversX blockchain fees, which are minimal.</p>
-        </li>
-        <li>
-          <h2 className="font-semibold">What kind of customer support does Lunar Pay offer?</h2>
-          <p className="mb-4">We offer robust customer support including a helpdesk, phone and email support to ensure our users have all the assistance they need.</p>
-        </li>
+        <ListItem 
+          title={'What is Lunar Pay?'} 
+          content={'Lunar Pay is a revolutionary financial platform on the MultiversX blockchain, designed to simplify and enhance financial operations in the Web3 ecosystem. It offers a smart contract-based wallet for managing financial transactions with enhanced flexibility and control.'}
+        />
+        <ListItem 
+          title={'What makes Lunar Pay secure?'}
+          content={"Lunar Pay prioritizes security by being non-custodial and permissionless. This means users have full control over their funds without needing extra permission to use the platform. Additionally, regular security audits and compliance with blockchain regulations enhance the platform's safety."}
+        />
+        <ListItem 
+          title={'Can Lunar Pay automate payroll processes?'}
+          content={'Absolutely. Lunar Pay uses the MultiversX blockchain to automate payroll, enabling businesses to efficiently distribute salaries and bonuses while ensuring security and compliance.'}
+        />
+        <ListItem 
+          title={'Does Lunar Pay support subscription management?'}
+          content={'Yes, Lunar Pay offers a crypto subscription solution for businesses, streamlining the process of generating subscription revenue and maximizing customer retention.'}
+        />
+        <ListItem 
+          title={'How can Lunar Pay assist in portfolio management?'}
+          content={'Lunar Pay facilitates efficient token transfers to multiple recipients and provides users with insights into their expenditures and anticipated earnings, aiding in effective portfolio management.'}
+        />
+        <ListItem 
+          title={'What is Lunar Pay Donations?'}
+          content={'Lunar Pay Donations is a feature designed for content creators, nonprofit organizations, and individuals to manage and accept recurring donations seamlessly on the MultiversX blockchain.'}
+        />
+        <ListItem 
+          title={'Who can benefit from using Lunar Pay?'}
+          content={'Lunar Pay is tailored for individuals, businesses, content creators, and nonprofit organizations looking for an efficient, secure, and user-friendly platform for managing financial transactions in the Web3 space.'}
+        />
+        <ListItem 
+          title={'Is Lunar Pay compliant with financial regulations?'}
+          content={'Yes, Lunar Pay is developed with a strong focus on compliance. We continually monitor regulatory changes and ensure that our platform adheres to the relevant laws and regulations in the blockchain and cryptocurrency sectors.'}
+        />
+        <ListItem 
+          title={'How can I start using Lunar Pay?'}
+          content={'Getting started with Lunar Pay is simple. Just sign up on our website, and you can begin managing your financial transactions immediately.'}
+        />
+        <ListItem 
+          title={'Are there any fees associated with using Lunar Pay?'}
+          content={'Lunar Pay charges a transaction fee of 0.5% per transaction. This fee structure is designed to sustain our platform while keeping it accessible to a wide range of users. Besides this, there are the MultiversX blockchain fees, which are minimal.'}
+        />
+        <ListItem 
+          title={'What kind of customer support does Lunar Pay offer?'}
+          content={'We offer robust customer support including a helpdesk, phone and email support to ensure our users have all the assistance they need.'}
+        />
       </ul>
     </div>
   )
