f26f358a4c0626571793d8a28318d53a3efb7f12
David Banciu <davidbanciu57@gmail.com>
2024-03-01 13:58:17 +0000

    Finished creating the new donation screen

--- src/screens/app/donations/components/donation-type-selector.tsx
+++ src/screens/app/donations/components/donation-type-selector.tsx
@@ -6,52 +6,58 @@ type Option = {
 }
 
 type Props = {
+  label?: string;
   optionsList: Option[];
   selectedOption: string;
   setSelectedOption: React.Dispatch<React.SetStateAction<string>>;
 }
 
 export const DonationTypeSelector = (props: Props) => {
-  const { optionsList, selectedOption, setSelectedOption } = props;
+  const { label, optionsList, selectedOption, setSelectedOption } = props;
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSelectedOption(event.target.value)
   }
 
   return (
-    <div className="flex space-x-4 mt-8 mb-8">
-      {optionsList.map((option) => {
-        const optionIsDisabled = option.label === 'recurringDonations' || option.label === 'charity'
-        const disabledStyle = !optionIsDisabled ? 'border-black' : ''
+    <div>
+      <div className="font-bold">
+        {label}
+      </div>
+      <div className="flex space-x-4">
+        {optionsList.map((option) => {
+          const optionIsDisabled = option.label === 'recurringDonations' || option.label === 'charity'
+          const disabledStyle = !optionIsDisabled ? 'border-black' : ''
 
-        return (
-          <Card className="flex flex-col flex-1 space-y-2 p-6">
-            <div className="flex items-center">
-              <div>
-                <input
-                  type="radio"
-                  value={option.label}
-                  checked={selectedOption === option.label}
-                  onChange={handleChange}
-                  className="sr-only"
-                  disabled={optionIsDisabled}
-                />
-                <div className={`w-5 h-5 rounded-full border mr-2 flex items-center justify-center ${disabledStyle}`}>
-                  {selectedOption === option.label && (
-                    <div className="w-3 h-3 rounded-full bg-black"></div>
-                  )}
+          return (
+            <Card className="flex flex-col flex-1 space-y-2 p-6">
+              <div className="flex items-center">
+                <div>
+                  <input
+                    type="radio"
+                    value={option.label}
+                    checked={selectedOption === option.label}
+                    onChange={handleChange}
+                    className="sr-only"
+                    disabled={optionIsDisabled}
+                  />
+                  <div className={`w-5 h-5 rounded-full border mr-2 flex items-center justify-center ${disabledStyle}`}>
+                    {selectedOption === option.label && (
+                      <div className="w-3 h-3 rounded-full bg-black"></div>
+                    )}
+                  </div>
+                </div>
+                <div>
+                  {displayLabel(option.label)}
                 </div>
               </div>
-              <div>
-                {displayLabel(option.label)}
+              <div className="text-sm text-muted-foreground">
+                {option.description}
               </div>
-            </div>
-            <div className="text-sm text-muted-foreground">
-              {option.description}
-            </div>
-          </Card>
-        )
-      })}
+            </Card>
+          )
+        })}
+      </div>
     </div>
   )
 }
--- src/screens/app/donations/create-donation-goal/create-donation-goal.tsx
+++ src/screens/app/donations/create-donation-goal/create-donation-goal.tsx
@@ -3,6 +3,11 @@ import { useState } from "react"
 import { ContainedScreen } from "@/components/prefab/contained-screen"
 import { Card } from "@/components/ui/card"
 import { DonationTypeSelector } from "../components"
+import { Token } from "@/core/tokens"
+import { useAccountTokensAvailableToDeposit } from "@/features/account-tokens/hooks"
+import { TokenSelectorWithAmount } from "@/core/tokens/components"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
 
 const donationTypes = [
   {label: 'oneTimeDonation', description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'},
@@ -14,23 +19,63 @@ const userTypes = [
 ]
 
 export const CreateDonationGoalScreen = () => {
-  const [selectedDonationType, setSelectedDonationType] = useState('oneTimeDonation')
-  const [selectedUserType, setSelectedUserType] = useState('contentCreator')
+  const [beneficiaryName, setBeneficiaryName] = useState('');
+  const [backgroundImageUrl, setBackgroundImageUrl] = useState('');
+  const [selectedDonationType, setSelectedDonationType] = useState('oneTimeDonation');
+  const [selectedUserType, setSelectedUserType] = useState('contentCreator');
+  const [selectedToken, setSelectedToken] = useState<Token | undefined>(undefined);
+  const [amount, setAmount] = useState('')
+
+  const tokens = useAccountTokensAvailableToDeposit();
   
   return (
     <ContainedScreen>
-      <Card className='p-6'>
+      <Card className='p-6 space-y-8'>
         <div className="text-xl font-semibold">New Donation</div>
         <DonationTypeSelector 
           optionsList={donationTypes}
           selectedOption={selectedDonationType}
           setSelectedOption={setSelectedDonationType}
         />
+        <div className={'flex-1'}>
+          <TokenSelectorWithAmount
+            tokens={tokens}
+            token={selectedToken}
+            onTokenChange={(token) => setSelectedToken(token)}
+            amount={amount}
+            onAmountChange={(amount) => setAmount(amount)}
+            hasMaxButton={false}
+            showBalances={false}
+            isAmountToReceive={true}
+          />
+        </div>
         <DonationTypeSelector 
+          label={'Page Layout'}
           optionsList={userTypes}
           selectedOption={selectedUserType}
           setSelectedOption={setSelectedUserType}
         />
+        <div>
+          <div className="font-bold">
+            Beneficiary Name
+          </div>
+          <Input
+            value={beneficiaryName}
+            placeholder="John Doe"
+            onChange={(e) => setBeneficiaryName(e.target.value)}
+          />
+        </div>
+        <div>
+          <div className="font-bold">
+            Background Image
+          </div>
+          <Input 
+            value={backgroundImageUrl}
+            placeholder="https://example.com/image.jpg"
+            onChange={(e) => setBackgroundImageUrl(e.target.value)}
+          />
+        </div>
+        <Button className='w-full'>Create New Donation</Button>
       </Card>
     </ContainedScreen>
   )
