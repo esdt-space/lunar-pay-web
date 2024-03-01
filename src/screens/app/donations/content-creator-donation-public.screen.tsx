b4cdc4a826cd4ee6e520bb14f7b1b3a76a5011f4
David Banciu <davidbanciu57@gmail.com>
2024-03-01 15:22:49 +0000

    Made the public donation page more responsive

--- src/screens/app/donations/content-creator-donation-public.screen.tsx
+++ src/screens/app/donations/content-creator-donation-public.screen.tsx
@@ -10,7 +10,7 @@ export const ContentCreatorDonationPublicScreen = () => {
       userHeroTag="@david"
       backgroundImage="https://images.unsplash.com/photo-1695718948137-1f4d1d5ba889?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTcwOTIyMzc2MQ&ixlib=rb-4.0.3&q=80&w=1080"
     >
-      <div className="flex flex-1 max-xl:flex-col">
+      <div className="flex flex-1 xl:space-x-8 max-xl:flex-col max-xl:space-y-8">
         <SingleDonationWidget />
         <LatestDonationsList />
       </div>
