ba527da5ded44e20443b3f81f1482cae5be8f0b3
David Banciu <davidbanciu57@gmail.com>
2024-03-07 09:45:52 +0000

    Replaced create-donation-goal with create-donation

--- src/screens/app/donations/index.ts
+++ src/screens/app/donations/index.ts
@@ -1,6 +1,6 @@
 export * from './content-creator-donation-public.screen';
 export * from './donations-list.screen';
 export * from './donation-dashboard.screen';
-export * from './create-donation-goal';
+export * from './create-donation';
 export * from './create-donation-widget';
 export * from './view-donation';
