ee26b8ec36d3dcca59763a234df064cb08791869
David Banciu <davidbanciu57@gmail.com>
2024-03-07 09:15:13 +0000

    Updated donation details component

--- src/screens/app/donations/view-donation/partials/donation-details.tsx
+++ src/screens/app/donations/view-donation/partials/donation-details.tsx
@@ -1,8 +1,12 @@
 import moment from 'moment';
+import { Copy } from 'lucide-react';
 import { FormatAmount } from '@multiversx/sdk-dapp/UI';
 
 import { useTokensMap } from '@/core/tokens';
 import { Donation } from '@/features/donations/models';
+import { Separator } from '@/components/ui/separator';
+import { Input } from '@/components/ui/input';
+import { useToast } from '@/components/ui/use-toast';
 
 type Props = {
   donation: Donation;
@@ -11,21 +15,52 @@ type Props = {
 export function DonationDetails(props: Props){
   const { donation } = props;
 
+  const { toast } = useToast()
+
   const tokensMap = useTokensMap();
   const token = tokensMap[donation.tokenIdentifier];
 
+  const publicDonationUrl = `https://localhost:5173/donations/${donation._id}/public`;
+
+  const copyButtonHandler = () => {
+    return navigator.clipboard.writeText(publicDonationUrl).then(() => {
+      toast({
+        description: 'Public donation Url copied to clipboard'
+      })
+    })
+  };
+
   return (
-    <div className='flex max-lg:flex-col justify-between gap-4'>
-      <div className={'text-muted-foreground text-sm'}>
-        Created on {moment(donation.createdAt).format('ll')}
+    <div className='flex flex-col space-y-6'>
+      <div className='flex flex-col'>
+        <div className={'text-muted-foreground text-sm'}>
+          Created on {moment(donation.createdAt).format('ll')}
+        </div>
+        <div className={'flex justify-between'}>
+          <div className='text-2xl font-black'>
+            <FormatAmount
+              token={token.identifier}
+              decimals={token.decimals}
+              value={donation.fixedAmount as string}
+            />
+          </div>
+        </div>
       </div>
-      <div className={'flex justify-between'}>
-        <div className='text-2xl font-black'>
-          <FormatAmount
-            token={token.identifier}
-            decimals={token.decimals}
-            value={donation.fixedAmount as string}
-          />
+
+      <div className='flex flex-col space-y-2'>
+        <Separator />
+        <div>
+          {donation.description}
+        </div>
+      </div>
+
+      <div className='flex flex-col space-y-4'>
+        <Separator />
+        <div className="relative flex w-full space-x-2 items-center">
+          <Input value={publicDonationUrl} readOnly />
+          <span className='absolute right-4 cursor-pointer' onClick={copyButtonHandler}>
+            <Copy className={'w-4 h-4'} />
+          </span>
         </div>
       </div>
     </div>
