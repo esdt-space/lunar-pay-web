9541e74e0e651d4311656818b961e3ad44dbe04a
DavidBanciu <67975174+BDavid57@users.noreply.github.com>
2024-05-10 09:05:29 +0100

    Added empty state to the donations list screen (#67)

--- src/screens/app/donations/donations-list.screen.tsx
+++ src/screens/app/donations/donations-list.screen.tsx
@@ -1,35 +1,31 @@
-import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { Plus } from "lucide-react";
 
 import { RoutesConfig } from "@/navigation";
 import { ContainedScreen } from "@/components/prefab/contained-screen";
-import { useEffect, useState } from "react";
 import { useDonationsCreatedQuery } from "@/features/donations/hooks/queries";
 import { Button } from "@/components/ui/button";
 import { DonationsListTable } from "@/features/donations/components";
-import { Card } from "@/components/ui/card";
+import { Card, CardContent } from "@/components/ui/card";
 import { PaginationButtons } from "@/components/shared/pagination";
+import { EmptyStateWithAction } from "@/components/shared/empty-states";
 
 export const DonationsListScreen = () => {
   const [currentPage, setCurrentPage] = useState(1);
-  const navigate = useNavigate();
 
   const {
     data: donationsResponse,
+    isFetched,
   } = useDonationsCreatedQuery(currentPage)
 
   const donationsList = donationsResponse?.data
   const numberOfPages = donationsResponse?.meta.totalPages
-  
-  useEffect(() => {
-    if (donationsList?.length === 0) {
-      navigate(RoutesConfig.createDonationIndex)
-    }
-  }, [donationsList?.length])
 
   const nextPageHandler = () => setCurrentPage(page => page + 1);
   const previousPageHandler = () => setCurrentPage(page => Math.max(1, page - 1));
+
+  const emptyDonationsList = isFetched && donationsList?.length === 0
 
   return (
     <ContainedScreen className="space-y-4">
@@ -43,13 +39,28 @@ export const DonationsListScreen = () => {
         </Button>
       </div>
       <Card>
-        <DonationsListTable donationsList={donationsList} />
-        <PaginationButtons 
-          previousPageHandler={previousPageHandler} 
-          nextPageHandler={nextPageHandler}
-          currentPage={currentPage}
-          lastPage={numberOfPages} 
-        />
+        <CardContent className="p-0">
+          {emptyDonationsList && (
+            <div className={'p-12'}>
+              <EmptyStateWithAction
+                title={'No donations created'}
+                description={"Donations that you create will appear here"}
+                action={<></>}
+              />
+            </div>
+          )}
+          {!emptyDonationsList && (
+            <div>
+              <DonationsListTable donationsList={donationsList} />
+              <PaginationButtons 
+                previousPageHandler={previousPageHandler} 
+                nextPageHandler={nextPageHandler}
+                currentPage={currentPage}
+                lastPage={numberOfPages} 
+              />
+            </div>
+          )}
+        </CardContent>
       </Card>
     </ContainedScreen>
   )
