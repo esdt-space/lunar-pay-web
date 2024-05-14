0b34fb96f377650500af3526e0e800c9acdadee3
David Banciu <davidbanciu57@gmail.com>
2024-05-14 16:49:24 +0300

    Made the first question be opened by default

--- src/screens/app/static-pages/frequently-asked-questions/components/list-item.tsx
+++ src/screens/app/static-pages/frequently-asked-questions/components/list-item.tsx
@@ -8,24 +8,30 @@ import { useState } from "react"
 
 type Props = {
   title: string,
-  content: string
+  content: string,
+  isFirst?: boolean,
 }
 
-export const ListItem = ({title, content}: Props) => {
-  const [activeItem, setActiveItem] = useState<string | null>(null);
+export const ListItem = ({title, content, isFirst}: Props) => {
+  const [activeItem, setActiveItem] = useState<string | undefined>(isFirst ? title : undefined);
 
   const handleToggle = (item: string) => {
-    setActiveItem(prevItem => (prevItem === item ? null : item));
+    setActiveItem(prevItem => (prevItem === item ? undefined : item));
   }
 
   const isActive = activeItem === title;
 
   return (
-    <Accordion type="single" collapsible className="w-full">
+    <Accordion 
+      type="single" 
+      collapsible 
+      className="w-full"
+      value={activeItem}
+      onValueChange={handleToggle}
+    >
       <AccordionItem 
         value={title} 
         className={`p-2 pl-4 pr-4 m-2 rounded-md ${isActive ? 'bg-slate-50' : ''}`}
-        onClick={() => handleToggle(title)}
       >
         <AccordionTrigger>{title}</AccordionTrigger>
         <AccordionContent>
