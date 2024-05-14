d0d3b4576a4179f7280d92dc29ae83b94b2a629f
David Banciu <davidbanciu57@gmail.com>
2024-05-14 12:59:49 +0300

    Re designed the FAQs screen

--- src/screens/app/static-pages/frequently-asked-questions/components/list-item.tsx
+++ src/screens/app/static-pages/frequently-asked-questions/components/list-item.tsx
@@ -1,13 +1,37 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
+import { useState } from "react"
+
 type Props = {
   title: string,
   content: string
 }
 
 export const ListItem = ({title, content}: Props) => {
+  const [activeItem, setActiveItem] = useState<string | null>(null);
+
+  const handleToggle = (item: string) => {
+    setActiveItem(prevItem => (prevItem === item ? null : item));
+  }
+
+  const isActive = activeItem === title;
+
   return (
-  <li>
-    <h2 className="font-semibold">{title}</h2>
-    <p className="mb-4">{content}</p>
-  </li>
+    <Accordion type="single" collapsible className="w-full">
+      <AccordionItem 
+        value={title} 
+        className={`p-2 pl-4 pr-4 m-2 rounded-md ${isActive ? 'bg-slate-50' : ''}`}
+        onClick={() => handleToggle(title)}
+      >
+        <AccordionTrigger>{title}</AccordionTrigger>
+        <AccordionContent>
+          {content}
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
   )
 }
