cc27b4013db029407bf82ef12e1564acf60590bb
David Banciu <davidbanciu57@gmail.com>
2024-06-05 10:47:11 +0300

    ED-218: Installed sdk-wallet

--- src/contracts/lunar-pay/subscriptions/interactions/create-subscription-interaction.ts
+++ src/contracts/lunar-pay/subscriptions/interactions/create-subscription-interaction.ts
@@ -7,11 +7,15 @@ import { lunarPaySmartContract } from "@/contracts/lunar-pay/contract-utils.ts";
 import { SubscriptionInteractionOptions } from "../types/index.ts";
 import { getSubscriptionTypeInteractionValue } from "../subscription-type-helper.ts";
 import { getSubscriptionAmountTypeInteractionValue, getCreateSubscriptionAmountInteractionValue } from "../subscription-amount-helper.ts";
+import { ApiNetworkProvider } from "@multiversx/sdk-network-providers";
 
+const apiNetworkProvider = new ApiNetworkProvider("https://devnet-api.multiversx.com");
 
 export async function createSubscriptionInteraction (options: SubscriptionInteractionOptions) {
   const sender = await getAddress();
   const { chainId } = getNetworkConfig();
+
+  const networkConfig = await apiNetworkProvider.getNetworkConfig();
 
   const interaction = lunarPaySmartContract.methods.createSubscription([
     new TokenIdentifierValue(options.token.identifier),
